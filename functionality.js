8e0bf3115c9d7972df928aca6d79bd551ea94d15
AndrewBresee <andrew.p.bresee@gmail.com>
2016-05-01 19:21:58 -0700

    Implement working light that changes based on click

--- functionality.js
+++ functionality.js
@@ -2,12 +2,26 @@ var light = document.getElementsByClassName('stopLight');
 var light = light[0];
 
 var red = document.getElementsByClassName('red'); 
-var yellow = document.getElementsByClassName('stopLight'); 
-var green = document.getElementsByClassName('stopLight'); 
+var yellow = document.getElementsByClassName('yellow'); 
+var green = document.getElementsByClassName('green'); 
 
 console.log("Light: ", light);
 
+var lightNum = 0; 
+
 light.onclick = function(){
-	red[0].setAttribute("style", "background: blue");
+	if (lightNum === 0) {
+		red[0].setAttribute("style", "background: none");	
+		green[0].setAttribute("style", "background: green");
+		lightNum++	
+	} else if (lightNum === 1) {
+		green[0].setAttribute("style", "background: none");
+		yellow[0].setAttribute("style", "background: yellow");
+		lightNum ++
+	} else {
+		red[0].setAttribute("style", "background: red");	
+		yellow[0].setAttribute("style", "background: none");
+		lightNum = 0;
+	}
 	console.log('CLICKED YELLOW!: ', yellow);
 }
