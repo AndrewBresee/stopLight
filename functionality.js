10988eaeb82a79209ea5972e921abbdd9692a7d7
AndrewBresee <andrew.p.bresee@gmail.com>
2016-05-01 19:16:34 -0700

    Document now has access to the different color lights on click

--- functionality.js
+++ functionality.js
@@ -1,1 +1,13 @@
-functionality.js
+var light = document.getElementsByClassName('stopLight'); 
+var light = light[0];
+
+var red = document.getElementsByClassName('red'); 
+var yellow = document.getElementsByClassName('stopLight'); 
+var green = document.getElementsByClassName('stopLight'); 
+
+console.log("Light: ", light);
+
+light.onclick = function(){
+	red[0].setAttribute("style", "background: blue");
+	console.log('CLICKED YELLOW!: ', yellow);
+}
