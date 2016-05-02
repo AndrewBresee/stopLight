2b3850ce07926cde8308001a38167df77bbd17ba
AndrewBresee <andrew.p.bresee@gmail.com>
2016-05-01 19:31:08 -0700

    Remove excess code

--- functionality.js
+++ functionality.js
@@ -1,26 +1,23 @@
-var light = document.getElementsByClassName('stopLight'); 
-var light = light[0];
+var light = document.getElementsByClassName('stopLight')[0]; 
 
-var red = document.getElementsByClassName('red'); 
-var yellow = document.getElementsByClassName('yellow'); 
-var green = document.getElementsByClassName('green'); 
-
-console.log("Light: ", light);
+var red = document.getElementsByClassName('red')[0]; 
+var yellow = document.getElementsByClassName('yellow')[0]; 
+var green = document.getElementsByClassName('green')[0]; 
 
 var lightNum = 0; 
 
 light.onclick = function(){
 	if (lightNum === 0) {
-		red[0].setAttribute("style", "background: none");	
-		green[0].setAttribute("style", "background: green");
+		red.setAttribute("style", "background: none");	
+		green.setAttribute("style", "background: green");
 		lightNum++	
 	} else if (lightNum === 1) {
-		green[0].setAttribute("style", "background: none");
-		yellow[0].setAttribute("style", "background: yellow");
+		green.setAttribute("style", "background: none");
+		yellow.setAttribute("style", "background: yellow");
 		lightNum ++
 	} else {
-		red[0].setAttribute("style", "background: red");	
-		yellow[0].setAttribute("style", "background: none");
+		red.setAttribute("style", "background: red");	
+		yellow.setAttribute("style", "background: none");
 		lightNum = 0;
 	}
 }
